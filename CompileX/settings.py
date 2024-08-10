e14804b469c2b2baf66468a1f138810680d86993
SkMishra <sanatkumarmishra02@gmail.com>
2024-08-11 00:07:42 +0530

    Initial Commit

--- CompileX/settings.py
+++ CompileX/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0e6x$^1+-9fk^(gz2e5noju#u@s7n5&i3lgmc_i*f*p2rr(%jj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
